fb31655df21300a70c408bbef4a6ac7b5626a920
Gennadiy Chernyshyk <genaloner@gmail.com>
2018-11-17 20:50:38 +0200

    Refactor code

--- src/qgittag.cpp
+++ src/qgittag.cpp
@@ -169,7 +169,7 @@ void QGitTag::parseReply()
     m_draft = release["draft"].toBool();
     m_prerelease = release["prerelease"].toBool();
 
-    foreach (const auto &asset, release["assets"].toArray())
+    foreach (const QJsonValue &asset, release["assets"].toArray())
         m_assets << QGitAsset(asset.toObject());
 
     m_error = NoError;
