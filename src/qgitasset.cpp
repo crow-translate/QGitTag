2f1c1f3e5895f55e0d91995c2cd74f057f3fa093
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-06-16 22:56:16 +0300

    Update copyrights

--- src/qgitasset.cpp
+++ src/qgitasset.cpp
@@ -1,21 +1,20 @@
 /*
- *  Copyright © 2018-2021 Hennadii Chernyshchyk <genaloner@gmail.com>
+ *  Copyright © 2018-2022 Hennadii Chernyshchyk <genaloner@gmail.com>
  *
- *  This file is part of Crow Translate.
+ *  This file is part of QGitTag.
  *
- *  Crow Translate is free software; you can redistribute it and/or modify
+ *  QGitTag is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  QGitTag is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU General Public License for more details.
  *
- *  You should have received a get of the GNU General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
+ *  You should have received a copy of the GNU General Public License
+ *  along with QGitTag. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "qgitasset.h"
